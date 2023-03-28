ed99a1d97383b8df9578358889151a859ec77efb
mjhans <mjhans83@gmail.com>
2023-03-28 22:20:00 +0900

    보강 수업 내용 추가

---
+++ appendix02/Guess.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Guess{
+    public static void main(String[] args){
+        int ch = 'l';
+
+        Scanner sc = new Scanner(System.in);
+        char input;
+        do {
+            System.out.print("문자 입력 : ");
+            input = sc.next().charAt(0);
+
+            if (input > ch){
+                System.out.println("down");
+            } else if (input < ch){
+                System.out.println("up");
+            } else {
+                System.out.println("equal");
+                break;
+            }
+        } while(input != '0');
+    }
+}
---
+++ appendix02/Sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자 입력 : ");
+        int num = sc.nextInt();
+        int sum = 0;
+
+        n * (n+1) / 2
+        // i를 증가하면서 더하는 로직
+        // for (int i = 1 ; i <= num ; i++){
+        //     sum += i;
+
+        // }
+        // 출력문
+        System.out.println(sum);
+    }
+}
