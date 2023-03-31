566fde590c06f5e9c0888dd707e118d6f7de0b95
mjhans <mjhans83@gmail.com>
2023-03-31 10:21:57 +0900

    array 추가

---
+++ ch05-arrayAndString/ArrayDemo.java
@@ -0,0 +1,9 @@
+public class ArrayDemo{
+    public static void main(String[] args){
+        int a = 4;
+        int[] intArray;
+        System.out.println(intArray[2]);
+        intArray = new int[5];
+        //System.out.println(intArray[2]);
+    }
+}
